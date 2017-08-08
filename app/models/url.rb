6e0bd24e52d70cfbd4d3c3be94a7a13ed8589d36
Haitham Mogherbi <haithammogherbi@Haithams-MacBook-Pro-2.local>
2017-08-08 14:35:24 +0800

    ``` created a shorten method -copied over my validations and shorten method from my bitly clone

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,5 @@
 class Url < ApplicationRecord
+  validates :long_url, uniqueness: true
+
+  validates :long_url, format: {with: (URI::regexp(['http', 'https'])), message: "entered is invalid url"}
 end
